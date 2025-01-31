49f43c6c94357097dc437b56181311975fb0945d
Giostatic <116467840+giostatic@users.noreply.github.com>
2025-01-31 13:23:25 -1000

    started work on Profile Page and created a Login Page

--- techforgood/src/pages/profile/ProfilePage.js
+++ techforgood/src/pages/profile/ProfilePage.js
@@ -1,6 +1,22 @@
+import {useSelector, useDispatch} from 'react-redux'
 
 const ProfilePage = () => {
-    return(<></>)
+    const userData = useSelector(state => state.user.user);
+    const dispatch = useDispatch();
+
+    return(
+    <>
+        <h1>Profile Page</h1>
+        {userData.userName || userData.name || userData.email ? (
+            <div>
+                <h3>Username: @{userData.username}</h3>
+                <h3>Name: {userData.name}</h3>
+                <h3>Email: {userData.email}</h3>
+            </div>
+        ) : (
+            <p>You are not logged in.</p>)}
+    </>
+    )
 }
 
 export default ProfilePage
