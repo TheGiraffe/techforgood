3489b29e3f70b9ff56677f1df4010933bc6514a0
giostatic <116467840+giostatic@users.noreply.github.com>
2025-02-09 13:07:28 -1000

    added upload image functionality to profile page and created login page skeleton linked to login authentication slice

--- techforgood/src/pages/login/LoginPage.js
+++ techforgood/src/pages/login/LoginPage.js
@@ -1,9 +1,50 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { login } from '../../features/auth/authSlice';
+
 const LoginPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        dispatch(login({ username, password }))
+            .unwrap()
+            .then(() => {
+                navigate('/profile');
+            });
+    };
+
     return (
         <div>
             <h1>Login Page</h1>
+            <form onSubmit={onSubmit}>
+                <label>
+                    Username:
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    />
+                </label>
+                <label>
+                    Password:
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </label>
+                <button type="submit">Login</button>
+            </form>
         </div>
     );
-}
+};
+
 
 export default LoginPage;
+
--- techforgood/src/pages/profile/ProfilePage.js
+++ techforgood/src/pages/profile/ProfilePage.js
@@ -1,20 +1,23 @@
 import {useSelector, useDispatch} from 'react-redux'
+import {useHistory} from 'react-router-dom'
 
 const ProfilePage = () => {
     const userData = useSelector(state => state.user.user);
     const dispatch = useDispatch();
+    const history = useHistory();
+
+    if(!userData) {
+        history.push('/login');
+    }
 
     return(
     <>
         <h1>Profile Page</h1>
-        {userData.userName || userData.name || userData.email ? (
-            <div>
-                <h3>Username: @{userData.username}</h3>
-                <h3>Name: {userData.name}</h3>
-                <h3>Email: {userData.email}</h3>
-            </div>
-        ) : (
-            <p>You are not logged in.</p>)}
+        <div>
+            <h3>Username: @{userData.username}</h3>
+            <h3>Name: {userData.name}</h3>
+            <h3>Email: {userData.email}</h3>
+        </div>
     </>
     )
 }
