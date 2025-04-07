02dedb3550ec0b2d44cc8d60d9ea4e0942c332bf
TheGiraffe <TheGiraffe@users.noreply.github.com>
2025-04-06 16:27:36 -1000

    changed font color to lighter green

--- techforgood/src/App.js
+++ techforgood/src/App.js
@@ -21,7 +21,7 @@ function AppContent() {
   return (
     <div className="App">
       <div>
-        <h1 style={{color: "green", fontSize: "8em"}}>Tech For Good</h1>
+        <h1 style={{color: "#61ba88", fontSize: "8em"}}>Tech For Good</h1>
         <h3>Look for your next tech opportunity(?)</h3>
       </div>
       <div>
