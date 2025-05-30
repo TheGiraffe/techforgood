db07f8f4ad88359c014286c2ee60b1efc6f11a4d
giostatic <116467840+giostatic@users.noreply.github.com>
2025-05-29 21:27:19 -1000

    created bid page where users bids will render and be able to see the requests that they submitted bids for

---
+++ techforgood/src/pages/bids/Bids.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react';
+import { useAuth } from '../../features/firebase/AuthProvider';
+
+const UserBids = () => {
+    
+    return (
+        <div>
+            <h1>User Bids</h1>
+            <p>This page will display the bids made by the user on various requests.</p>
+            <p>yada yada hello world</p>
+        </div>
+    );
+}
+
+export default UserBids;
